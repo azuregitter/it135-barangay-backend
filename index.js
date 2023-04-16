d8dd037166479a410da9bfa639fb5f4b42857590
azuregitter <geromeriego@gmail.com>
2023-04-17 00:41:04 +0800

    CRD of CRUD created

--- index.js
+++ index.js
@@ -77,6 +77,97 @@ app.get("/checkuser/:username", (req, res) => {
     return res.json(data);
   });
 });
+app.get("/checkuserdocs/:username", (req, res) => {
+  const q = "SELECT * FROM Forms WHERE username = ?";
+  console.log("look at it: " + req.params.username);
+  db.query(q, [req.params.username],(err, data) => {
+    if (err) {
+      console.log(err);
+      return res.json(err);
+    }
+    console.log(data);
+    return res.json(data);
+  });
+});
+
+app.get("/viewuserdocs/:username/:id/:type", (req, res) => {
+  let values = [
+    req.params.username,
+    req.params.id,
+  ];
+  let q;
+  if (req.params.type === 'Proof of Residence'){
+    q = "select * from Forms INNER JOIN Residence on Forms.FormID = Residence.ResidenceID WHERE username = ? and FormID = ?";
+  }else if (req.params.type === 'Indigence Certification'){
+    q = "select * from Forms INNER JOIN Indigent on Forms.FormID = Indigent.IndigentID WHERE username = ? and FormID = ?";
+  }
+  db.query(q, [req.params.username, req.params.id],(err, data) => {
+    if (err) {
+      console.log(err);
+      return res.json(err);
+    }
+    console.log(data);
+    return res.json(data);
+  });
+});
+
+app.post("/submitresident", (req, res) => {
+  let q = "BEGIN";
+  console.log('begin residence');
+  db.query(q);
+  q = "INSERT INTO Forms (`FormType`, `username`,`fullname`,`AppDate`,`Status`) VALUES(?)";
+  let values = [
+    req.body.FormType,
+    req.body.username,
+    req.body.fullname,
+    req.body.AppDate,
+    req.body.Status,
+  ];
+  db.query(q, [values], (err, data) => {
+    if (err) return res.send(err);
+  });
+  values = [
+    req.body.fullname,
+    req.body.Address,
+  ];
+  q = "INSERT INTO Residence (ResidenceID, Name, Address) VALUES(LAST_INSERT_ID(),?)";
+  db.query(q, [values], (err, data) => {
+    if (err) return res.send(err);
+  });
+  console.log('commit residence');
+  q = "Commit";
+  db.query(q);
+});
+
+app.post("/submitindigent", (req, res) => {
+  let q = "BEGIN";
+  console.log('begin indigent');
+  db.query(q);
+  q = "INSERT INTO Forms (`FormType`, `username`,`fullname`,`AppDate`,`Status`) VALUES(?)";
+  let values = [
+    req.body.FormType,
+    req.body.username,
+    req.body.fullname,
+    req.body.AppDate,
+    req.body.Status,
+  ];
+  db.query(q, [values], (err, data) => {
+    if (err) return res.send(err);
+  });
+  values = [
+    req.body.fullname,
+    req.body.Address,
+    req.body.RName,
+  ];
+  q = "INSERT INTO Indigent (IndigentID, PName, PAddress, RName) VALUES(LAST_INSERT_ID(),?)";
+  db.query(q, [values], (err, data) => {
+    if (err) return res.send(err);
+  });
+  q = "Commit";
+  console.log('commit indigent');
+  db.query(q);
+});
+
 
 app.post("/accounts", (req, res) => {
   const q = "INSERT INTO accounts(`firstname`, `middlename`, `lastname`, `sex`,`dateofbirth`, `civilstatus`, `phonenumber`, `email`, `address`, `username`, `password`, `privilege`) VALUES (?)";
@@ -116,6 +207,30 @@ app.post("/books", (req, res) => {
     if (err) return res.send(err);
     return res.json(data);
   });
+});
+
+app.delete("/deletedocs/:id/:type", (req, res) => {
+  const value = req.params.id;
+  let q;
+  if (req.params.type === 'Proof of Residence'){
+    q = " DELETE FROM Residence WHERE ResidenceID = ? ";
+    console.log(q);
+  }else if (req.params.type === 'Indigence Certification'){
+    q = " DELETE FROM Indigent WHERE IndigentID = ? ";
+    console.log(q);
+  }
+
+  db.query(q, [value], (err, data) => {
+    if (err) return res.send(err);
+    //return res.json(data);
+  });
+  q = " DELETE FROM Forms WHERE FormID = ? ";
+
+  db.query(q, [value], (err, data) => {
+    if (err) return res.send(err);
+    //return res.json(data);
+  });
+
 });
 
 app.delete("/books/:id", (req, res) => {
